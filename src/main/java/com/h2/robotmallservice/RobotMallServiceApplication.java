3e4015c7caf98b840f2b6fb96962b620c518933c
hechangfeng <515018620@qq.com>
2022-03-06 18:02:59 +0800

    1.初始化Springboot项目

---
+++ src/main/java/com/h2/robotmallservice/RobotMallServiceApplication.java
@@ -0,0 +1,13 @@
+package com.h2.robotmallservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotMallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotMallServiceApplication.class, args);
+    }
+
+}
